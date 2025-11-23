5c50992a795fc4858e5350d7cd0c28d4c45f7466
Juan Segura <nachosegura600@gmail.com>
2025-11-23 04:09:50 -0300

    Arreglo

--- src/main/java/com/magneto/mutants/dto/DnaRequest.java
+++ src/main/java/com/magneto/mutants/dto/DnaRequest.java
@@ -2,6 +2,8 @@ package com.magneto.mutants.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.magneto.mutants.validation.ValidDnaSequence;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 
 public class DnaRequest {
@@ -15,9 +17,13 @@ public class DnaRequest {
     public DnaRequest(String[] adn) {
         this.adn = adn;
     }
-
-    public String[] getAdn() {
-        return adn;
+    @Schema(
+            description = "Secuencia de ADN (matriz NxN) con solo caracteres A, T, C, G.",
+            example = "["ATGCGA","CAGTGC","TTATGT","AGAAAG","CCCCTA","TCACTG"]",
+            required = true
+    )
+    public static String[] getAdn(DnaRequest dnaRequest) {
+        return dnaRequest.adn;
     }
 
     public void setAdn(String[] adn) {
