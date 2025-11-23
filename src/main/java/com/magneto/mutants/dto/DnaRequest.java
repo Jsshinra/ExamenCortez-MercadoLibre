a309555f6c3b79bce724efbf5ddd98ef8210a06c
Juan Segura <nachosegura600@gmail.com>
2025-11-23 04:49:19 -0300

    ejemplo

--- src/main/java/com/magneto/mutants/dto/DnaRequest.java
+++ src/main/java/com/magneto/mutants/dto/DnaRequest.java
@@ -1,9 +1,20 @@
 package com.magneto.mutants.dto;
 
 import com.magneto.mutants.validation.ValidDnaSequence;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(example = "{\n" +
+        "  \"dna\": [\n" +
+        "    \"ATGCGA\",\n" +
+        "    \"CAGTGC\",\n" +
+        "    \"TTATGT\",\n" +
+        "    \"AGAAAG\",\n" +
+        "    \"CCCCTA\",\n" +
+        "    \"TCACTG\"\n" +
+        "  ]\n" +
+        "}")
 public class DnaRequest {
 
     @ValidDnaSequence
